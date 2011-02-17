1110943bd3de90e77deac75c5b6b804b5cfe8573
nephilim <i015005@gmail.com>
2011-02-17 11:10:54 +0900

    TestCase 최초 작성

---
+++ src/main/java/nephilim/study/spring/ch0305/Calculator.java
@@ -0,0 +1,10 @@
+package nephilim.study.spring.ch0305;
+
+public class Calculator {
+
+	public int calcSum(String path) {
+		
+		return 0;
+	}
+
+}
---
+++ src/test/java/nephilim/study/spring/ch0305/CalcSumTest.java
@@ -0,0 +1,14 @@
+package nephilim.study.spring.ch0305;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class CalcSumTest {
+	
+	@Test
+	public void sumAllNumbers() {
+		Calculator calculator = new Calculator();
+		int sum = calculator.calcSum( getClass().getResource("numbers.txt").getPath() );
+		assertEquals("파일에서 읽은 수의 합이 150여야 함", (10+20+30+40+50),sum);
+	}
+}
