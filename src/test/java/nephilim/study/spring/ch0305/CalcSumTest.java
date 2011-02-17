a4494006d1b769b760525ebf708ab78f2946c553
nephilim <i015005@gmail.com>
2011-02-17 11:15:10 +0900

    Template을 적용하지 않은 버전 (with try - catch -finally)

--- src/test/java/nephilim/study/spring/ch0305/CalcSumTest.java
+++ src/test/java/nephilim/study/spring/ch0305/CalcSumTest.java
@@ -1,12 +1,15 @@
 package nephilim.study.spring.ch0305;
 
 import static org.junit.Assert.*;
+
+import java.io.IOException;
+
 import org.junit.Test;
 
 public class CalcSumTest {
 	
 	@Test
-	public void sumAllNumbers() {
+	public void sumAllNumbers() throws IOException {
 		Calculator calculator = new Calculator();
 		int sum = calculator.calcSum( getClass().getResource("numbers.txt").getPath() );
 		assertEquals("파일에서 읽은 수의 합이 150여야 함", (10+20+30+40+50),sum);
